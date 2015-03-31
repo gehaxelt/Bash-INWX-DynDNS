3dcd4b2aeffac3c228bd910542be237223bcc34f
Johannes Visintini <github@joker234.de>
2015-04-01 01:43:51 +0200

    Added IPv6 Support.
    Your IPv6 Address is also changing. This will be useful for all People which
    uses IPv6 and IPv4. If no IPv6 is detected (p.e. because curlmyip6.com is not
    finishing), the second if-term is never executed and there should be no Problem
    for IPv4-only User.

--- dnsupdate.sh
+++ dnsupdate.sh
@@ -2,15 +2,25 @@
 
 USERNAME="" #INWX USERNAME
 PASSWORD="" #INWX PASSWORD
-DNSID="" #DNS Entry ID
+DNSIDv4="" #DNS Entry ID
+DNSIDv6="" #DNS Entry ID
 APIHOST="https://api.domrobot.com/xmlrpc/"
-OLDIP=$(cat old.ip)
-NEWIP=$(curl -s curlmyip.com)
+OLDIPv4=$(cat old.ipv4)
+OLDIPv6=$(cat old.ipv6)
+NEWIPv4=$(curl -s curlmyip.com)
+NEWIPv6=$(curl -s curlmyip6.com)
 
-if [ ! "$OLDIP" == "$NEWIP" ]; then
-    echo $NEWIP > old.ip
+if [ ! "$OLDIPv4" == "$NEWIPv4" ]; then
+    echo $NEWIP > old.ipv4
     echo "Updating IPv4..." >> update.log
-    DATA=$(cat update.api | sed "s/%PASSWD%/$PASSWORD/g;s/%USER%/$USERNAME/g;s/%DNSID%/$DNSID/g;s/%NEWIP%/$NEWIP/g")
+    DATA=$(cat update.api | sed "s/%PASSWD%/$PASSWORD/g;s/%USER%/$USERNAME/g;s/%DNSID%/$DNSIDv4/g;s/%NEWIP%/$NEWIPv4/g")
+    curl  -s -X POST -d "$DATA" "$APIHOST" --header "Content-Type:text/xml" >> update.log
+fi
+
+if [ ! "$OLDIPv6" == "$NEWIPv6" ]; then
+    echo $NEWIP > old.ipv6
+    echo "Updating IPv6..." >> update.log
+    DATA=$(cat update.api | sed "s/%PASSWD%/$PASSWORD/g;s/%USER%/$USERNAME/g;s/%DNSID%/$DNSIDv6/g;s/%NEWIP%/$NEWIPv6/g")
     curl  -s -X POST -d "$DATA" "$APIHOST" --header "Content-Type:text/xml" >> update.log
 fi
 
